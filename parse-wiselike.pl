09eba36c5c48fd8b82e80cd9012a127a09120b82
Audrey Tang <audreyt@audreyt.org>
2017-03-22 08:01:41 +0800

    * Merge from @targee

--- parse-wiselike.pl
+++ parse-wiselike.pl
@@ -1,14 +1,16 @@
 use 5.12.0;
 #  • 科技新聞通訊社-林鴻仁's avatar image.科技新聞通訊社-林鴻仁 asked Audrey Tang:
+##    Audrey Tang's avatar image.Audrey Tang•Jan 26Taiwan’s Digital Minister
+#  • Anonymous asked Audrey Tang:
+##    Anonymous profile image
+##    Anonymous•Dec 27
 use utf8;
 my ($id, $title, $user, $body, @replies);
 my ($r_user, $r_body, $r_date);
 my @parsed;
 open my $fh, '<:utf8', "wiselike-2017-01-30-backup.txt";
 while (<$fh>) {
-    #• Anonymous asked Audrey Tang:
-   #Werjer Ting's avatar image.Werjer Ting asked Audrey Tang:
-    if (/(?:avatar image.|• (?=Anonymous))(.+) asked .+:/) {
+    if (/• (.+) asked Audrey Tang:/) {
         if ($r_user) { push @replies, { user => $r_user, date => $r_date, body => $r_body }; }
         if (@replies) {
             push @parsed, { id => $id, title => $title, user => $user, replies => [@replies] };
@@ -16,19 +18,23 @@ while (<$fh>) {
         }
         $id = $title = $user = $body = $r_user = $r_body = $r_date = '';
         $user = $1;
+        $user =~ s/.*avatar image.//;
         chomp($title = <$fh>) until $title;
         $title =~ s/^\s*//;
     }
-    elsif (/avatar image\.(.+?)•([A-Za-z]+?\s\d+)/) {
+    elsif (/(.+?)•([A-Za-z]+?\s\d+)/) {
         if ($r_user) { push @replies, { user => $r_user, date => $r_date, body => $r_body }; }
         $r_user = $1;
         $r_date = $2;
         $r_body = '';
+        $r_user =~ s/.*avatar image\.|^\s*//;
+        $r_date =~ s/Jan\s(\d+)/Jan $1 2017/;
+        $r_date =~ s/([A-Za-z]+?\s\d+)$/$1 2016/;
     }
     elsif (m{\[http://wslk.io/(.+)\]}) {
         $id = $1;
     }
-    elsif (m!Appreciated? \(\d+|'s avatar image|\[\s{10}\s*\]|•Edit!) {
+    elsif (m!Appreciated? \(\d+|'s avatar image|\[\s{10}\s*\]|•Edit|Anonymous profile image|Appreciate•!) {
         next;
     }
     elsif ($r_user) {
@@ -46,5 +52,5 @@ if (@replies) { push @parsed, { id => $id, title => $title, user => $user, repli
 use JSON;
 print encode_json(\@parsed);
 __DATA__
-# "id", "title", "user", "body", "replies": [ "user", "body" ]
+# "id", "title", "user", "body", "replies": [ "user", "body", "date" ]
 
