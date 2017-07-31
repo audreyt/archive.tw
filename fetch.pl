3b2407c1cd57bf33827d5110afbbc479478ba3e4
Audrey Tang <audreyt@audreyt.org>
2017-07-31 14:41:49 +0800

    * Add fetch script

---
+++ fetch.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+use 5.12.0;
+use YAML::XS 'Dump';
+use XML::Simple 'XMLin';
+my $url = shift or die "Usage: $0 <hackpad-url>";
+my $xml = `curl http://pad.archive.tw:8080/$url`;
+my $heading = XMLin($xml)->{debate}{debateBody}{debateSection}{heading};
+$heading =~ s/\s+/-/g;
+open(OUT, "| perl add-links.pl > $heading.an.xml");
+print OUT $xml;
+close OUT;
+exec("vim $heading.an.xml && git add $heading.an.xml");
