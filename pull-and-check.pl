70476abc5fbac9a6bb18f6f30bf4fd4819a75a90
Audrey Tang <audreyt@audreyt.org>
2017-09-30 23:10:08 +0800

    * Fix utf-8 filenames and HTTPS checkout

--- pull-and-check.pl
+++ pull-and-check.pl
@@ -3,18 +3,19 @@ use warnings;
 
 die "upload.pl needs to be present in the current directory" unless -e 'upload.pl';
 
-my $fh = open '<', '.git/HEAD' or die "Please run $0 in a checked-out github repo with .an.xml files";
+open my $fh, '<', '.git/HEAD' or die "Please run $0 in a checked-out github repo with .an.xml files";
 <$fh> =~ m{ref: refs/heads/(.+)} or die "Cannot parse current branch";
 my $branch = $1;
 close $fh;
 
-`git remote -v` =~ m{github\.com:(.*?)\.git \(fetch\)} or die "Cannot parse remote (needs to be in github)";
+`git remote -v` =~ m{github\.com[/:](.*?)\.git \(fetch\)} or die "Cannot parse remote (needs to be in github)";
 my $baseURL = "https://raw.githubusercontent.com/$1/$branch";
+
+`git config core.quotepath off`;
 
 while (1) {
     if (`git pull` =~ /up-to-date/) { sleep 60; next; }
-    my @xmls = `git show --pretty=format: --name-only HEAD | grep .an.xml\$` or exit;
-    for (@xmls) { chomp; system($^X, "upload.pl" => $_) if -s; }
-
+    my @xmls = grep { -s } `git show --pretty=format: --name-only HEAD | grep .an.xml\$` or exit;
+    for (@xmls) { chomp; system($^X, "upload.pl" => $_); }
     system "rm -rf /var/cache/nginx/*; service nginx reload";
 }
