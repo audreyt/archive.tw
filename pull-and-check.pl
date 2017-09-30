3d0010298d8f91b8f6f5ffc507698bfe79922fba
Audrey Tang <audreyt@audreyt.org>
2017-09-30 23:11:09 +0800

    * Add baseURL

--- pull-and-check.pl
+++ pull-and-check.pl
@@ -1,5 +1,6 @@
 use 5.12.0;
 use warnings;
+use URI::Escape;
 
 die "upload.pl needs to be present in the current directory" unless -e 'upload.pl';
 
@@ -9,13 +10,13 @@ my $branch = $1;
 close $fh;
 
 `git remote -v` =~ m{github\.com[/:](.*?)\.git \(fetch\)} or die "Cannot parse remote (needs to be in github)";
-my $baseURL = "https://raw.githubusercontent.com/$1/$branch";
+my $baseURL = "https://raw.githubusercontent.com/$1/$branch/";
 
 `git config core.quotepath off`;
 
 while (1) {
     if (`git pull` =~ /up-to-date/) { sleep 60; next; }
     my @xmls = grep { -s } `git show --pretty=format: --name-only HEAD | grep .an.xml\$` or exit;
-    for (@xmls) { chomp; system($^X, "upload.pl" => $_); }
+    for (@xmls) { chomp; system($^X, "upload.pl" => ($baseURL . uri_escape($_))); }
     system "rm -rf /var/cache/nginx/*; service nginx reload";
 }
