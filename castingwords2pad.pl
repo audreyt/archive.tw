dce8d0ca9603426aaa616b285539caf9512f1ea0
Audrey Tang <audreyt@audreyt.org>
2017-12-03 00:46:45 +0800

    * Add first-to-full-name autocompletion.

--- castingwords2pad.pl
+++ castingwords2pad.pl
@@ -1,11 +1,23 @@
 #!/usr/bin/env perl
 use utf8;
+use 5.14.0;
 print ~~<>;
 binmode STDOUT, ':utf8';
+my %names;
 while (<>) {
     utf8::decode($_);
     next if /^\s*$/;
     next if /crosstalk/;
+    if (/((?:\s\w\.|[-'\w ]+)*):/) {
+        my $n = $1;
+        if ($n =~ / /) {
+            $names{$n =~ s/ .*//r} = $n;
+        }
+        elsif (my $fullname = $names{$n}) {
+            s/(?:\s\w\.|[-'\w ]+)*:/$fullname:/;
+        }
+
+    }
     print (
         (s/^\[(.*)\]/($1)/ or s/^(?:\s\w\.|[-'\w ]+)*\K: +/:\n    /)
             ? $/ : "    "
