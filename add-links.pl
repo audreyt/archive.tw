4096fef1f1258c0614f5757fa7379f803358942d
Audrey Tang <audreyt@audreyt.org>
2018-08-07 00:29:29 +0800

    * Allow full-width parens in semi-markdown
      This is because Etherpad's URL auto-linking feature currently
      breaks currently on ） but not )

--- add-links.pl
+++ add-links.pl
@@ -1,7 +1,8 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl -C
+use utf8;
 while (<>) { print
-    s{\[([^\]]+)\]\((https?:[^\)]+)\)}{<a href="$2">$1</a>}rg =~
-    s{\[([^\]]+)\]\(<a href="(https?:[^"]+)">[^\)]+\)}{<a href="$2">$1</a>}rg =~
+    s{\[([^\]]+)\][（\(](https?:[^\)]+)[\)）]}{<a href="$2">$1</a>}rg =~
+    s{\[([^\]]+)\][（\(]<a href="(https?:[^"]+)">[^\)）]+[\)）]}{<a href="$2">$1</a>}rg =~
     s{(?<!href=")(https?[-=_;&?.:/#%0-9a-zA-Z]+)}{<a href="$1">$1</a>}rg =~
     s{DIGI(\+|＋)}{DIGI⁺}r
 }
