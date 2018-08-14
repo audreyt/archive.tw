7f4fbd4c9a97df4617aa21b7b2a7e91b61ccf1e3
Audrey Tang <audreyt@audreyt.org>
2018-08-15 00:13:56 +0800

    * Non-greedy capture of links

--- add-links.pl
+++ add-links.pl
@@ -1,8 +1,10 @@
-#!/usr/bin/env perl -C
+#!/usr/bin/env perl
 use utf8;
-while (<>) { print
-    s{\[([^\]]+)\][（\(](https?:[^\)]+)[\)）]}{<a href="$2">$1</a>}rg =~
-    s{\[([^\]]+)\][（\(]<a href="(https?:[^"]+)">[^\)）]+[\)）]}{<a href="$2">$1</a>}rg =~
+while (<>) {
+    utf8::decode($_);
+    print
+    s{\[([^\]]+)\][（\(](https?:[^\)]+?)[\)）]}{<a href="$2">$1</a>}rg =~
+    s{\[([^\]]+)\][（\(]<a href="(https?:[^"]+?)">[^\)）]+[\)）]}{<a href="$2">$1</a>}rg =~
     s{(?<!href=")(https?[-=_;&?.:/#%0-9a-zA-Z]+)}{<a href="$1">$1</a>}rg =~
     s{DIGI(\+|＋)}{DIGI⁺}r
 }
