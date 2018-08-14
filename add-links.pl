1c400a8ce07059f078b8466d42d82e6254c3e7bc
Audrey Tang <audreyt@audreyt.org>
2018-08-15 00:19:38 +0800

    * Fix utf8 mode

--- add-links.pl
+++ add-links.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/env perl
 use utf8;
+binmode STDOUT, ':utf8';
 while (<>) {
     utf8::decode($_);
     print
-    s{\[([^\]]+)\][（\(](https?:[^\)]+?)[\)）]}{<a href="$2">$1</a>}rg =~
     s{\[([^\]]+)\][（\(]<a href="(https?:[^"]+?)">[^\)）]+[\)）]}{<a href="$2">$1</a>}rg =~
+    s{\[([^\]]+)\][（\(](https?:[^\)]+?)[\)）]}{<a href="$2">$1</a>}rg =~
     s{(?<!href=")(https?[-=_;&?.:/#%0-9a-zA-Z]+)}{<a href="$1">$1</a>}rg =~
     s{DIGI(\+|＋)}{DIGI⁺}r
 }
